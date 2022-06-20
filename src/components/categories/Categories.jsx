2b78becaff707733f31245ac7b3308b126496441
Александр Невский <sashnevski@MacBook-Air-Aleksandr.local>
2022-06-20 20:47:22 +0300

    catregories

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -1,15 +1,33 @@
 import React from "react";
 
 const Categories = () => {
+  const [activeCategory, setActiveCategory] = React.useState(0);
+
+  const changeCategory = (idx) => {
+    setActiveCategory(idx);
+  };
+
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((category, index) => (
+          <li
+            key={category}
+            className={index === activeCategory ? "active" : null}
+            onClick={() => changeCategory(index)}
+          >
+            {category}
+          </li>
+        ))}
       </ul>
     </div>
   );
