dd37835f565a50d7fd089b45c84cc0e077f61b63
Александр Невский <sashnevski@MacBook-Air-Aleksandr.local>
2022-06-22 21:47:19 +0300

    small adaptive

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -17,19 +17,17 @@ const Categories = () => {
   ];
 
   return (
-    <div className="categories">
-      <ul>
-        {categories.map((category, index) => (
-          <li
-            key={category}
-            className={index === activeCategory ? "active" : null}
-            onClick={() => changeCategory(index)}
-          >
-            {category}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul className="categories">
+      {categories.map((category, index) => (
+        <li
+          key={category}
+          className={index === activeCategory ? "active" : null}
+          onClick={() => changeCategory(index)}
+        >
+          {category}
+        </li>
+      ))}
+    </ul>
   );
 };
 
