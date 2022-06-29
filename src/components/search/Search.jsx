05499a42ddd1c7060ef65d53ba099d4eac640bcb
Александр Невский <sashnevski@MacBook-Air-Aleksandr.local>
2022-06-29 17:51:24 +0300

    add focus on input during clear input

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { useRef } from "react";
 import classes from "./search.module.scss";
 
 const Search = ({ value, setValue }) => {
+  const inputRef = useRef();
+
+  const toClearInput = () => {
+    setValue("");
+    inputRef.current.focus();
+  };
+
   return (
     <div className={classes.control}>
       <label htmlFor="search" className={classes.searchIcon}>
@@ -10,6 +18,7 @@ const Search = ({ value, setValue }) => {
         </svg>
       </label>
       <input
+        ref={inputRef}
         className={classes.input}
         value={value}
         onChange={(e) => setValue(e.target.value)}
@@ -18,11 +27,7 @@ const Search = ({ value, setValue }) => {
         placeholder="Найти пиццу..."
       />
       {value && (
-        <button
-          className={classes.clear}
-          type="button"
-          onClick={() => setValue("")}
-        >
+        <button className={classes.clear} type="button" onClick={toClearInput}>
           <svg width="20" height="20">
             <use href="/img/sprite.svg#clear"></use>
           </svg>
