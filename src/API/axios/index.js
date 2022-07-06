b320d87947c2f8343bc87b223404f048faf7225e
Александр Невский <sashnevski@MacBook-Air-Aleksandr.local>
2022-07-06 13:45:12 +0300

    create axios config and servises

---
+++ src/API/axios/index.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const BASE_URL = "https://62b208e0c7e53744afc67927.mockapi.io";
+
+const $http = axios.create({
+  baseURL: BASE_URL,
+});
+
+$http.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
---
+++ src/API/services/itemsService.js
@@ -0,0 +1,10 @@
+import { $http } from "../axios";
+
+export default class ItemsService {
+  LIMIT_ITEMS = 8;
+  getItems(title, category, sortBy, order, page) {
+    return $http.get(
+      `/items?title=${title}&category=${category}&sortBy=${sortBy}&order=${order}&page=${page}&limit=${LIMIT_ITEMS}`
+    );
+  }
+}
